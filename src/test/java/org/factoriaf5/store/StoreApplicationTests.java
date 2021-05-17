b2e00676130bac784e4e63d4ef43a742171f30b5
yassscoder <yasminlcg@gmail.com>
2021-05-17 21:44:12 +0200

    initial commit

---
+++ src/test/java/org/factoriaf5/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
