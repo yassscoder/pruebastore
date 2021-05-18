e75e75d3c7f31e7472fc47846f198fb541ffb31e
yassscoder <yasminlcg@gmail.com>
2021-05-18 15:36:03 +0200

    adding products to store

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -1,10 +1,32 @@
 package org.factoriaf5.store.controllers;
 
+import org.factoriaf5.store.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ProductController {
+
+    public List<Product> products = new ArrayList<>();
+
+        @GetMapping("/products")
+        public List<Product> allProducts() {
+            return products;
+        }
+
+        @PostMapping("/products")
+
+    public void addProduct(Product product){
+            products.add(product);
+
+        }
+
+
+
     @GetMapping("/health")
     public String check () {
         return "Hello! I'm a server";
---
+++ src/main/java/org/factoriaf5/store/model/Product.java
@@ -0,0 +1,11 @@
+package org.factoriaf5.store.model;
+
+public class Product {
+    public String name;
+    public Float price;
+
+    public Product(String name, Float price) {
+        this.name = name;
+        this.price = price;
+    }
+}
